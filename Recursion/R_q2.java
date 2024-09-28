ecbe37676d0be43e03c336342bf769a3c2cd2766
Vedant Gadge <vedant.gadgegsis@gmail.com>
2024-09-28 12:58:31 +0530

    .

---
+++ Recursion/R_q2.java
@@ -0,0 +1,10 @@
+package Recursion;
+//Print sum of first n natural nos.
+public class R_q2 {
+    int R_Sum(int i,int n, int s){
+        
+    }
+    public static void main(String[] args) {
+        int n = 10;
+    }
+}
