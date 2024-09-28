41440b0091cdb14c8910e033e950d790142d694a
Vedant Gadge <vedant.gadgegsis@gmail.com>
2024-09-28 14:18:41 +0530

    .

--- Recursion/R_q2.java
+++ Recursion/R_q2.java
@@ -1,10 +1,12 @@
 package Recursion;
 //Print sum of first n natural nos.
 public class R_q2 {
-    int R_Sum(int i,int n, int s){
-        
+    static int  R_Sum(int n, int s){
+        if(n==0) return s;      
+        return s += R_Sum( n--, s);
     }
     public static void main(String[] args) {
         int n = 10;
+        System.out.println(R_Sum(n,0));
     }
 }
